57bf1ee8bc68491dfa56c30c687d823a321dc4ab
alexKwonIsAwesome <khwalex@gmail.com>
2016-05-31 07:53:22 +0900

    update news view: added search form

--- wpsblog/wpsblog/views.py
+++ wpsblog/wpsblog/views.py
@@ -21,6 +21,13 @@ def news(request):
     response = requests.get("https://watcha.net/home/news.json?page=1&per=50")
     news_dict = json.loads(response.text)
     news_list = news_dict.get("news")
+
+    form_html = """
+    <form method = "GET" action="/news/">
+        <input type="text" name="search">
+        <input type="submit" value="검색">
+    </form>
+    """
     
     if search:
         news_list = list(filter(
@@ -29,6 +36,7 @@ def news(request):
             ))
     content = "<h1>News</h1>" + \
             "<p>This is news page.</p>" +\
+            form_html +\
             "<p>{count}개의 영화 뉴스 정보가 있습니다. </p>".format(count=len(news_list)) + \
             "".join([
                 "<h2>{title}</h2><img src={image_src}><p>{content}</p>".format(
