63fd45f7b7032e7c3fc080ce48e7e79d1365a373
alexKwonIsAwesome <khwalex@gmail.com>
2016-05-30 20:44:24 +0900

    update views.py: added html tag on the home function

--- wpsblog/wpsblog/views.py
+++ wpsblog/wpsblog/views.py
@@ -3,7 +3,7 @@ import requests
 from django.http.response import HttpResponse
 
 def home(request):
-    return HttpResponse("hello world")
+    return HttpResponse("<h1>hello world</h1><p>This is home page</p>")
 
 def room(request, room_id):
     url = "https://api.zigbang.com/v1/items?detail=true&item_ids=" + room_id
